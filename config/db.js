ead0c9e24031f554be7e0d9be83e209ad7c87e65
Sandun Dissanayake <java2012@gmail.com>
2020-06-22 18:23:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 var db = {
 	pool: mysql.createPool({
-		connectionLimit: process.env.BID_DB_POOL_SIZE,
-		host: process.env.BID_DB_HOST,
-		user: process.env.BID_DB_USER,
-		password: process.env.BID_DB_PASSWORD,
-		database: process.env.BID_DB_DATABASE,
+		connectionLimit: process.env.DB_POOL_SIZE,
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
 		debug: false
 	}),
 	query: function (qry, params, next) {
